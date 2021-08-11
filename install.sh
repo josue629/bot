b894ef6a5e0b790d382a48d35692415852647e8f
arcadia <85562683+josue629@users.noreply.github.com>
2021-08-11 12:19:29 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ apt install libwebp -y
 apt install tesseract -y
 apt install ffmpeg -y
 apt install wget -y
+apt install python
+apt install python2
+apt install python3
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm i 
 npm start
